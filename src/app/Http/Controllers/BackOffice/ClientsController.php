7af426ae0328756e0fc8490cfccc82390041ddd0
Imjad abdessamad <a.imjad@boomrank.fr>
2020-06-25 13:39:09 +0100

    25 06 commit

--- src/app/Http/Controllers/BackOffice/ClientsController.php
+++ src/app/Http/Controllers/BackOffice/ClientsController.php
@@ -3,82 +3,43 @@
 namespace App\Http\Controllers\BackOffice;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use DB;
 
-class ClientsController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+class ClientsController extends Controller {
+
+    public function __construct() {
+
+        $this->middleware('jwt', ['except' => ['signup']]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function all() {
+
+        $id = $this->guard()->user()->boutique_id;
+
+        if($id) {
+
+            $Utilisateurs = DB::table('utilisateurs')
+            ->join('profils', 'profils.id', '=', 'utilisateurs.profil_id')
+            ->where('utilisateurs.boutique_id', '=', $id)
+            ->where('utilisateurs.role_id', '=', 3)
+            ->select('utilisateurs.email', 'utilisateurs.status', 'profils.*')
+            ->get();
+
+        }
+
+        return $Utilisateurs;
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function guard() {
+
+        return Auth::guard();
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
